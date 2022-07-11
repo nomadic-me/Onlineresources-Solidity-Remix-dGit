e2181c9a5dd875aa484bb5df9446c28b5f68c04c
"Satish Surath: RemixIDE" <"s@n-m.co">
2022-07-11 19:06:51 -0400

    Week 20 day 3 Activity 1

--- Week20-Day3-Activity1.sol
+++ Week20-Day3-Activity1.sol
@@ -4,10 +4,13 @@ contract BankAccount {
     address payable accountOwner = 0xc3879B456DAA348a16B6524CBC558d2CC984722c;
 
     function withdraw(uint amount, address payable recipient) public {
+        // Here the Require means that its cheaper than an IF Statement
         require(recipient == accountOwner, "You don’t own this account!");
         return recipient.transfer(amount);
     }
-
+    // There is no logic because, if you make something payable 
+    // and any value added to the contract ("like a deposit" to a contract)
+    // So by having external  
     function deposit() public payable {}
 
     function() external payable {}
